8fcf7c66656c89edd1af774a8040d419ccd9cc5f
Michael Kim <m.kim4247@gmail.com>
2018-11-26 23:08:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(year){
+    return year - this.startDate.getFullYear()
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled(){
+    let startVertical = parseInt(this.beginningLocation.vertical);
+    let endVertical = parseInt(this.endingLocation.vertical)
+    let vertical = Math.abs(startVertical - endVertical)
+
+    let startHor = eastWest[this.beginningLocation.horizontal];
+    let endHor = eastWest[this.endingLocation.horizontal];
+    let horizontal = Math.abs(startHor - endHor)
+
+    let total = vertical + horizontal
+    return total
+  }
+
+  estimatedTime(peak=false){
+    if (peak){
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
+  }
+}
+
+let eastWest = {'1st Avenue': 1, '2nd Avenue': 2, '3rd Avenue': 3, 'Lexington Avenue': 4, 'Park': 5, 'Madison Avenue': 6, '5th Avenue': 7}
