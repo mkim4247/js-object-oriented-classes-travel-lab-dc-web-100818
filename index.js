af1b3d50ed68443fa44ed84f982efa2fbb779fd6
Michael Kim <m.kim4247@gmail.com>
2020-06-08 19:53:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
+
 class Driver {
   constructor(name, startDate){
     this.name = name;
     this.startDate = new Date(startDate);
   }
-
-  yearsExperienceFromBeginningOf(year){
-    return year - this.startDate.getFullYear()
+  
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
   }
 }
 
 class Route {
-  constructor(beginningLocation, endingLocation){
+  //initialize a route with 2 arguments. Both arguments have attributes horizontal and vertical.
+  constructor(beginningLocation, endingLocation) {
     this.beginningLocation = beginningLocation;
     this.endingLocation = endingLocation;
   }
-
-  blocksTravelled(){
-    let startVertical = parseInt(this.beginningLocation.vertical);
-    let endVertical = parseInt(this.endingLocation.vertical)
-    let vertical = Math.abs(startVertical - endVertical)
-
-    let startHor = eastWest[this.beginningLocation.horizontal];
-    let endHor = eastWest[this.endingLocation.horizontal];
-    let horizontal = Math.abs(startHor - endHor)
-
-    let total = vertical + horizontal
-    return total
+  
+  blocksTravelled() {
+    let verticalDist = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+    let horizontalDist = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+    return verticalDist + horizontalDist;
   }
-
-  estimatedTime(peak=false){
-    if (peak){
-      return this.blocksTravelled()/2
-    } else {
-      return this.blocksTravelled()/3
+  
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled()/2;
+    }
+    else {
+      return this.blocksTravelled()/3;
     }
   }
 }
 
-let eastWest = {'1st Avenue': 1, '2nd Avenue': 2, '3rd Avenue': 3, 'Lexington Avenue': 4, 'Park': 5, 'Madison Avenue': 6, '5th Avenue': 7}
+
